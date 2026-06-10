66da0a95bd5e68910363426c4c5d23ae1768b737
suehuynh <nguyenhuynh01.nh@gmail.com>
2026-06-10 09:16:37 -0400

    feat: update config with large and small config

--- config/config.py
+++ config/config.py
@@ -10,18 +10,31 @@ class Config:
     "length": ("length_split","length"),
     "addprim_jump": ("add_prim_split", "addprim_jump"),
     }
-    max_seq_len: int = 50
-    device: str = "cuda" if torch.cuda.is_available() else "cpu"
+    
 
-    # Model
-    d_model: int = 768
-    d_head: int = 64
-    d_mlp: int = 3072
-    n_layers: int = 12
-    n_heads: int = 12
+    device: str = "cuda" if torch.cuda.is_available() else "cpu"
+    d_vocab: int = 25
+
+    # Small Model
+    max_seq_len: int = 128 
+    d_model: int = 128
+    d_head: int = 32
+    d_mlp: int = 512
+    n_layers: int = 2
+    n_heads: int = 4
+    
+    # Large Model
+    # max_seq_len: int = 50
+    # d_model: int = 768
+    # d_head: int = 64
+    # d_mlp: int = 3072
+    # n_layers: int = 12
+    # n_heads: int = 12
+
     init_range: float = 0.02
     layer_norm_eps: float = 1e-5
-    n_ctx: int = 1024
+    # n_ctx: int = 1024
+    n_ctx: int = 128
 
     # Training
     batch_size: int = 32
